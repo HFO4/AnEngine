062d2e68ea272a35810b407072f11cea40334367
HFO4 <912394456@qq.com>
2018-10-19 13:27:31 +0800

    Remove useless code

--- AnEngine/ManagedTask.cpp
+++
@@ -1,9 +0,0 @@
-#include "ManagedTask.hpp"
-#include "onwind.h"
-#include <map>
-using namespace std;
-
-namespace AnEngine::Utility
-{
-
-}
--- AnEngine/ThreadPool.cpp
+++
@@ -1,9 +0,0 @@
-#include "ThreadPool.hpp"
-
-namespace AnEngine::Utility::ThreadPool
-{
-	namespace Private
-	{
-		ThreadPool u_s_threadPool(std::thread::hardware_concurrency());
-	}
-}
